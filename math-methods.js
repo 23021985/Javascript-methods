d8ec3224884879927954dac0653448df85a20e74
23021985 <h.de.vreugd@hotmail.com>
2021-04-12 20:08:08 +0200

    huiswerk af

--- math-methods.js
+++ math-methods.js
@@ -8,12 +8,20 @@ console.log(Math.PI);
 // Bereken de omtrek van een cirkel met een diameter van 4 en log dit in de terminal. De eenheid (cm, meters, etc. maakt niet uit).
 // Tip: de formule is omtrek = diameter * pi
 // ---- Verwachte uitkomst: 12.566370614359172
+function circle(diameter){
+return diameter * Math.PI;
+}
+
+console.log(circle("4"));
 
 
 /* Opdracht 2: */
 // Bereken de omtrek van een cirkel met een diameter van 12 en rond dit af naar een heel getal. Log dit in de terminal.
 // ---- Verwachte uitkomst: 38
-
+function circle2(diameter){
+    return diameter * Math.PI
+}
+console.log(Math.round(circle2("12")));
 
 
 /* Opdracht 3: */
@@ -21,6 +29,7 @@ console.log(Math.PI);
 // ---- Verwachte uitkomst: 3
 
 
+console.log(Math.sqrt(9));
 
 /* Opdracht 4: */
 // Genereer een willekeurig getal tussen de 0 en 1 en doe dan een check:
@@ -28,3 +37,16 @@ console.log(Math.PI);
 // * is het willekeurige getal kleiner dan 0.5 ? Log dan het getal met "Het glas is half leeg" in de terminal
 // Tip: iedere keer als je het script opnieuw draait (wel even een extra spatie toevoegen o.i.d.) is dit getal anders ;)
 // ---- Verwachte uitkomst: Bijbehorende zin afhankelijk van de uitkomst
+
+
+// console.log(Math.random())
+const outcome = Math.random()
+function check(outcome){
+
+     if (outcome < 0.5){
+         return outcome + " Het glas is half leeg"
+     } else {
+         return outcome + " Het glas is half vol"
+     }
+}
+console.log(check(outcome));
--- string-methods.js
+++ string-methods.js
@@ -4,7 +4,7 @@
 
 const word = "zoetsappig";
 console.log(word.length);
-
+console.log("\n");
 
 /* Opdracht 1 */
 //  Check of het woord "Banaan" langer is dan het woord "Aardappel" en log dit in de terminal.
@@ -12,14 +12,22 @@ console.log(word.length);
 
 const fruit = "Banaan";
 const vegetable = "Aardappel";
-
-
+console.log(fruit.length > vegetable.length)
+console.log("\n");
 
 /* Opdracht 2 */
 //  Zorg ervoor dat de laatste letter van het woord in fruit en vegetable (hierboven) in de terminal gelogd wordt.
 //  Tip: bedenk 1 manier die voor beide woorden zou werken.
 // ---- Verwachte uitkomst voor fruit: "n"
 // ---- Verwachte uitkomst voor vegetable: "l"
+
+function lastChar(string) {
+    return string.substr(string.length - 1);
+}
+console.log(lastChar(fruit))
+console.log("\n");
+
+
 
 
 
@@ -28,22 +36,23 @@ const vegetable = "Aardappel";
 // ---- Verwachte uitkomst: "De kat krabt de krullen van de trap"
 
 const userInput = "  De kat krabt de krullen van de trap ";
-
-
+console.log(userInput.trim())
+console.log("\n");
 
 /* Opdracht 4 */
 // Check of het woord "Fantastisch" in onderstaande zin voorkomt en log dit in de terminal.
 // ---- Verwachte uitkomst: true
 
 const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch, nietwaar?"
-
-
+console.log(story.includes("Fantastisch"))
+console.log("\n");
 
 /* Opdracht 5 */
 // Knip de vraag van bovenstaande tekst (story) af, zodat alleen het eerste gedeelte overblijft en log dit in de terminal.
 // ---- Verwachte uitkomst: "We vinden de behaalde resultaten werkelijk prachtig."
-
-
+console.log(story.substr(0, 52))
+console.log("\n");
+console.log(story.substring(0, (story.indexOf("F")-1))); //story.indexOf("F")-1) zoekt naar de F in de string.
 
 /* Opdracht 6 */
 // Maak van onderstaand bedrag een Nederlands bedrag door het volgende te doen:
@@ -53,7 +62,16 @@ const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch,
 // ---- Verwachte uitkomst: "€35,59"
 
 const price = "$35.59";
+console.log("\n");
 
+console.log(price.replace("$","€").replace(".",","));
+
+// price.replace(".",",");
+// console.log(price);
+// console.log("\n");
+// console.log(price.replace("$","€") && price.replace(".",","));
+// console.log("\n");
+// console.log(price.replace("$","€") || price.replace(".",","));
 
 
 
